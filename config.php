3a4dddc29025d4b92ecca230048316c2db865395
Josué Francisco <josuefrancisco2017@outlook.pt>
2019-11-24 22:28:55 +0100

    refator autoload

--- config.php
+++ config.php
@@ -1,11 +1,21 @@
 <?php
 
-function __autoload ($className) {
+function autoloadClass ($className) {
 
-    var_dump($className);
-    require_once ("$className.php");
+    if (file_exists($className.".php") === true) {
+        require_once ($className.".php");
+    }
 
 }
+
+spl_autoload_register("autoloadClass");
+spl_autoload_register(function ($className) {
+
+    if (file_exists("Abstrata". DIRECTORY_SEPARATOR .$className.".php") === true) {
+        require_once ("Abstrata". DIRECTORY_SEPARATOR .$className.".php");
+    }
+
+});
 
 
 ?>
